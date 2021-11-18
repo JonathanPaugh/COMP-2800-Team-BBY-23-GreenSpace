919c46f5a2ebdaf722b7348ea977074fee49ebd8
Anna Yujeong An <yujeong99an@gmail.com>
2021-11-17 17:21:00 -0800

    Updated error messages in plant search and twit pages

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,13 @@
 // Firebase Initialization //
 
 firebase.initializeApp({
-    apiKey: "AIzaSyC7xuP2DGbB4a0aBT0AcLUDOMnuxoRpGKg",
-    authDomain: "green-space-32378.firebaseapp.com",
-    projectId: "green-space-32378",
-    storageBucket: "green-space-32378.appspot.com",
-    messagingSenderId: "475716211072",
-    appId: "1:475716211072:web:c95affba8e5c4c907d7165"
+    apiKey: "AIzaSyAQo0bWQ82MncRmQB5noR5SYhwaecGBvzM",
+    authDomain: "green-space-692b9.firebaseapp.com",
+    projectId: "green-space-692b9",
+    storageBucket: "green-space-692b9.appspot.com",
+    messagingSenderId: "42156484612",
+    appId: "1:42156484612:web:6faf6d7b3e22689e6e6d51",
+    measurementId: "G-4L310WQLGN"
 });
 
 const database = firebase.firestore();
